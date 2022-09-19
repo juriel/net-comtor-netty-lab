7769eff4a0a91bb2ad3542ef8268a9fc53430363
juriel <jaimeurieltorres@gmail.com>
2022-09-19 00:38:22 -0500

    first commit

---
+++ src/main/java/net/comtor/net/comtor/netty/lab/Main.java
@@ -0,0 +1,13 @@
+package net.comtor.net.comtor.netty.lab;
+
+/**
+ *
+ * @author juriel
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        
+        System.err.println("HOLA");
+    }
+}
